cba92ba0624f13f64839e35dc77883f8da00dde2
Tom Vandenberk <tomm-v@live.co.uk>
2011-06-19 20:04:34 +0100

    added a speed variable, and bounce feature

--- main.rb
+++ main.rb
@@ -8,13 +8,14 @@ require "square"
 
 width = 960
 height = 640
+speed = 2
 
 squares = [
-  Square.new(width, height, Gosu::Color::RED),
-  Square.new(width, height, Gosu::Color::GREEN),
-  Square.new(width, height, Gosu::Color::BLUE),
-  Square.new(width, height, Gosu::Color::YELLOW),
-  Square.new(width, height, Gosu::Color::FUCHSIA),
+  Square.new(width, height, speed, Gosu::Color::RED),
+  Square.new(width, height, speed, Gosu::Color::GREEN),
+  Square.new(width, height, speed, Gosu::Color::BLUE),
+  Square.new(width, height, speed, Gosu::Color::YELLOW),
+  Square.new(width, height, speed, Gosu::Color::FUCHSIA),
 ]
 
 Game.new(width, height, squares).show
--- square.rb
+++ square.rb
@@ -1,24 +1,36 @@
 class Square
           
-  def initialize(width, height, colour)
+  def initialize(width, height, speed, colour)
     @width = width
     @height = height
+    @speed = speed
     @colour = colour
-    @x1 = rand(@width)
-    @y1 = rand(@height)
-    @x2 = rand(@width)
-    @y2 = rand(@height)
+    @x1 = rand(@width)  ; @x1_dir =  speed
+    @y1 = rand(@height) ; @y1_dir =  speed
+    @x2 = rand(@width)  ; @x2_dir = -speed
+    @y2 = rand(@height) ; @y2_dir = -speed
   end
-  
+
   def draw(window)
+
+    # step 1: draw current lines
     window.draw_line(@x1, 0, @colour, @width, @y1, @colour)
     window.draw_line(@width, @y1, @colour, @x2, @height, @colour)
     window.draw_line(@x2, @height, @colour, 0, @y2, @colour)
     window.draw_line(0, @y2, @colour, @x1, 0, @colour)
-    @x1 += 1
-    @y1 += 1
-    @x2 -= 1
-    @y2 -= 1
+
+    # step 2: calculate new coordinates
+    @x1 += @x1_dir
+    @y1 += @y1_dir
+    @x2 += @x2_dir
+    @y2 += @y2_dir
+
+    # step 3: bounce when we reach the edge
+    @x1_dir *= -1 if @x1 >= @width || @x1 <= 0
+    @y1_dir *= -1 if @y1 >= @height || @y1 <= 0
+    @x2_dir *= -1 if @x2 >= @width || @x2 <= 0
+    @y2_dir *= -1 if @y2 >= @height || @y2 <= 0
+
   end
   
 end
